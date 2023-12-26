1b33c6082b014e87a5fc4974950db0c7eba52b08
Evan Kerik <ekerik220@gmail.com>
2023-12-26 14:15:59 +0900

    add google login

---
+++ src/server/api/auth/lucia.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import { lucia } from "lucia";
+import { env } from "~/env";
+import { planetscale } from "@lucia-auth/adapter-mysql";
+import { google } from "@lucia-auth/oauth/providers";
+import { connection } from "~/server/db";
+import { userKeys, userSessions, users } from "~/server/db/schema";
+import { getBaseUrl } from "~/utils/api";
+import { nextjs_future } from "lucia/middleware";
+
+export const auth = lucia({
+  env: env.NODE_ENV === "production" ? "PROD" : "DEV",
+  adapter: planetscale(connection, {
+    user: "t3-twitter-clone_user",
+    key: "t3-twitter-clone_user_key",
+    session: "t3-twitter-clone_user_session",
+  }),
+  middleware: nextjs_future(),
+  getUserAttributes: (data) => {
+    return {
+      username: data.username,
+    };
+  },
+});
+
+export const googleAuth = google(auth, {
+  clientId: env.GOOGLE_AUTH_CLIENT_ID,
+  clientSecret: env.GOOGLE_AUTH_CLIENT_SECRET,
+  redirectUri: `${getBaseUrl()}${env.GOOGLE_AUTH_REDIRECT_PATH}`,
+});
+
+export type Auth = typeof auth;
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -6,10 +6,12 @@
  * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
  * need to use are documented accordingly near the end.
  */
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+import { type Session } from "lucia";
 import superjson from "superjson";
 import { ZodError } from "zod";
+import { auth } from "~/server/api/auth/lucia";
 
 import { db } from "~/server/db";
 
@@ -21,7 +23,9 @@ import { db } from "~/server/db";
  * These allow you to access things when processing a request, like the database, the session, etc.
  */
 
-type CreateContextOptions = Record<string, never>;
+type CreateContextOptions = {
+  session: Session | null;
+};
 
 /**
  * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
@@ -36,6 +40,7 @@ type CreateContextOptions = Record<string, never>;
 const createInnerTRPCContext = (_opts: CreateContextOptions) => {
   return {
     db,
+    session: _opts.session,
   };
 };
 
@@ -45,8 +50,11 @@ const createInnerTRPCContext = (_opts: CreateContextOptions) => {
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = (_opts: CreateNextContextOptions) => {
-  return createInnerTRPCContext({});
+export const createTRPCContext = async (_opts: CreateNextContextOptions) => {
+  const { req, res } = _opts;
+  const authRequest = auth.handleRequest({ req, res });
+  const session = await authRequest.validate();
+  return createInnerTRPCContext({ session });
 };
 
 /**
@@ -93,3 +101,15 @@ export const createTRPCRouter = t.router;
  * are logged in.
  */
 export const publicProcedure = t.procedure;
+
+const enforceUserIsAuthed = t.middleware(async ({ ctx, next }) => {
+  if (!ctx.session) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({
+    ctx: { ...ctx, session: ctx.session },
+  });
+});
+
+export const privateProcedure = t.procedure.use(enforceUserIsAuthed);
